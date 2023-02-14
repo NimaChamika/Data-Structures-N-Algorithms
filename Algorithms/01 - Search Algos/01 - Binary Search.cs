b3e7ff3dd6105e46e294420521abc7c88a164b8a
Nirmal Chamika <lynchamika@gmail.com>
2023-02-14 06:47:43 +0530

    +algo - 1-search algo - 2-binary search added

---
+++ Algorithms/01 - Search Algos/01 - Binary Search.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DnA.Algorithms._01___Search_Algos
+{
+    class _01___Binary_Search
+    {
+        internal static void Solution()
+        {
+            List<int> l = new List<int>() {1,5,9,10,15,23,46,78,90 };
+            int searchElement = 12;
+            Console.WriteLine(BinarySearch(l, searchElement));
+        }
+
+        private static int BinarySearch(List<int> lst,int value)
+        {
+            int l = 0;
+            int r = lst.Count-1;
+
+            while(l<=r)
+            {
+                int mid = (l + r) / 2;
+
+                if(lst[mid] < value)
+                {
+                    l = mid+1;
+                }
+                else if(lst[mid] > value)
+                {
+                    r = mid-1;
+                }
+                else
+                {
+                    return mid;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using DnA.Algorithms._01___Search_Algos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DnA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            _01___Binary_Search.Solution();
+        }
+
+
+    }
+}
